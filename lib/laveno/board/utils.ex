2dea51871f5c3515609e571ff1e824d049c28674
Ricardo C <ricardo@suggestic.com>
2022-08-05 20:56:45 -0500

    add knight moves and valid_move?/2 and /3

--- lib/laveno/board/utils.ex
+++ lib/laveno/board/utils.ex
@@ -1,5 +1,6 @@
 defmodule Laveno.Board.Utils do
   use Bitwise
+  require Logger
 
   @offset_row 49
   @offset_column 97
@@ -27,7 +28,7 @@ defmodule Laveno.Board.Utils do
 
 
   def moves(:knight, square_offset) do
-    [
+    moves_list_binaries = [
       <<(1 <<< (square_offset + 8 + 2))::size(64)>>,
       <<(1 <<< (square_offset + 8 - 2))::size(64)>>,
       <<(1 <<< (square_offset - 8 + 2))::size(64)>>,
@@ -37,6 +38,8 @@ defmodule Laveno.Board.Utils do
       <<(1 <<< (square_offset - 16 + 1))::size(64)>>,
       <<(1 <<< (square_offset - 16 - 1))::size(64)>>,
     ]
+    Enum.reduce(moves_list_binaries, <<0::size(64)>> |> :binary.decode_unsigned(),
+    fn m, acc -> acc ||| ( m |> :binary.decode_unsigned() ) end)
   end
 
   def which_piece?(
@@ -46,7 +49,15 @@ defmodule Laveno.Board.Utils do
     c = column - @offset_column
     r = row - @offset_row
     offset = 64 - 8 * r - c - 1
-    mask = <<1 <<< offset::size(64)>> |> :binary.decode_unsigned()
+    which_piece?(board, offset)
+  end
+
+  def which_piece?(
+        board = %{bb: bb},
+        offset_to_square
+      ) do
+
+    mask = <<1 <<< offset_to_square::size(64)>> |> :binary.decode_unsigned()
 
     piece =
       Enum.find(
@@ -57,7 +68,28 @@ defmodule Laveno.Board.Utils do
     piece
   end
 
+  def valid_move?(board,
+                  square = <<c1::size(8), r1::size(8), c2::size(8), r2::size(8)>>
+      ) do
+    c_from = c1 - @offset_column
+    r_from = r1 - @offset_row
+    c_to = c2 - @offset_column
+    r_to = r2 - @offset_row
+    offset_from = 64 - 8 * r_from - c_from - 1
+    offset_to = 64 - 8 * r_to - c_to - 1
+    valid_move?(board, offset_from, offset_to)
+  end
+
   def valid_move?(board, offset_from, offset_to) do
-    false
+    piece = which_piece?(board, offset_from)
+    moves = moves(piece, offset_from)
+    offset_to_mask = (<<1 <<< offset_to::size(64)>> |> :binary.decode_unsigned())
+    IO.inspect(piece)
+    IO.inspect(offset_to_mask)
+    case (moves &&& <<1 <<< offset_to::size(64)>> |> :binary.decode_unsigned()) != 0 do
+      true -> true
+      _ -> false
+    end
+
   end
 end
