914e7ec3f8f1f00d900cdfbb07fc453a12802adb
Ricardo C <ricardo@suggestic.com>
2022-08-09 23:01:19 -0500

    add documentation options

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule Laveno.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "l a v e n o  ♜",
+      homepage_url: "https://laveno.one",
+      docs: [main: "l a v e n o ♟"]
     ]
   end
 
@@ -22,8 +25,8 @@ defmodule Laveno.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      {:ex_doc, "~> 0.27", only: :dev, runtime: false}    
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 end
